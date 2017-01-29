2e319cf4b1336a975e51ac81e2bbc46cf2f10131
jasonhui <huihangkin@gmail.com>
2017-01-29 17:43:56 +0800

    showcase of ngrx's store

---
+++ login.reducer.ts
@@ -0,0 +1,43 @@
+interface OnProcess {
+  current: 'onProcess';
+}
+
+interface OnSuccess {
+  current: 'onSuccess';
+  payload: { username: string };
+}
+
+interface OnFailure {
+  current: 'onFailure';
+  error: Error;
+}
+
+export type LoginStateT = OnProcess | OnSuccess | OnFailure | null;
+
+
+export function loginReducer(state: LoginStateT = null, action: Actions): LoginStateT {
+  switch (action.type) {
+    case ActionTypes.LOGIN_REQUEST:
+      return {
+        current: 'onProcess'
+      };
+
+    case ActionTypes.LOGIN_SUCCESS:
+      return {
+        current: 'onSuccess',
+        payload: { username: (<LoginSuccessAction>action).username }
+      };
+
+    case ActionTypes.LOGIN_FAILURE:
+      return {
+        current: 'onFailure',
+        error: (<LoginFailureAction>action).error
+      };
+
+    case ActionTypes.LOGIN_RESET:
+      return initialState;
+
+    default:
+      return state;
+  }
+}
